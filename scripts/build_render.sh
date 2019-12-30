f7df41324695c268715f926f83e600eac4b7f6aa
Christopher Kelly <chris@nourishbalancethrive.com>
2019-12-30 12:48:17 -0800

    build render

--- scripts/build_render.sh
+++ scripts/build_render.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 pip install -r requirements.txt
 python manage.py migrate
-#nodeenv -p --node=12.14.0
-#npm install
-#npm run build-assets
+nodeenv -p --node=12.14.0
+unset NODE_ENV
+npm install
+npm run build-assets
